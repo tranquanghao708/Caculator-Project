10eaa9ba9e7125b172c960a9c03b782a7a68006d
SL.Carnkhra <secretssrt@gmail.com>
2026-09-02 21:36:13 +0700

    thêm transmit arg vaddr ở Rho

--- header/Argorithm_Cal.h
+++ header/Argorithm_Cal.h
@@ -1,5 +1,5 @@
 //hàm nhân tố số nguyên tố
-long long Pollard_s_Rho(long long input);
+long long Pollard_s_Rho(long long input, long long *r_gcd);
 
 //Ước chung lớn nhất
 long long gcd(long long input1, long long input2);
